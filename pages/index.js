f961aaa07b16206b8baf31c2b1b70e61557f2a77
Achal Jain <achal.jain@publicissapient.com>
2020-04-12 21:29:55 +0530

    add source code for hacker news

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import './styles.css';
+import FeedView from '../src/components/Feedview';
 
 const HackerNews = () => (
   <div>
-    <h1>Hacker News</h1>
-    <p>WIP...</p>
+    <FeedView />
   </div>
 );
 
